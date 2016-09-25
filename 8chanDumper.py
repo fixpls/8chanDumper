a90b160b6e547f8bd954e6a95f5dcd663621ad10
fixpls <fixpls@openmailbox.org>
2016-09-25 22:25:26 +0100

    Initialize Repository

---
+++ 8chanDumper.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# If you encounter any problems please make an issue on  or make a new thread on /test/
+
+import requests, time, os, sys, math, argparse
+from os import listdir
+from os.path import isfile, join
+from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
+
+parser = argparse.ArgumentParser(description="Directory dump to 8chan with multiple-file posts")
+
+parser.add_argument('directory', action="store", type=str, help="Path to the directory with the files you wish to dump")
+parser.add_argument('board', action="store", type=str, help="8chan board you wish to post to (e.g. for /test/ put test)")
+parser.add_argument('thread', action="store", type=int, help="Thread number to post to (e.g. for https://8ch.net/test/res/34429.html put 34429)")
+
+results = parser.parse_args()
+
+if results.thread == None:
+    print(results)
+    exit()
+
+DIRECTORY = results.directory # e.g. "/home/test/pictures"
+BOARD = results.board # e.g. "test"
+THREAD = str(results.thread) # e.g. "34429"
+SUBJECT = ""
+EMAIL = ""
+NAME = ""
+HIDE_FLAG = "on"
+POST_WAIT = 8
+ALLOWED_FILE_TYPES = (".jpg", ".jpeg", ".gif", ".png", ".webm", ".mp4", ".swf", ".pdf")
+
+current_file = 0
+
+
+def file_upload_monitor(monitor):
+	sys.stdout.write("\r     " + str(monitor.bytes_read) + " out of " + str(monitor.len) + " Bytes Uploaded... ")
+	sys.stdout.flush
+
+# Get a sorted list of filenames
+filenames = [f for f in listdir(DIRECTORY) if isfile(join(DIRECTORY, f)) and f.endswith(ALLOWED_FILE_TYPES)]
+filenames.sort()
+
+print("\nDumping from directory \"" + DIRECTORY + "\" to https://8ch.net/" + BOARD + "/res/" + THREAD + ".html")
+print("Files to upload: " + str(len(filenames)) + ", Posts to make: " + str(math.ceil(len(filenames) / 5)) + "\n")
+
+while current_file < len(filenames):
+	# Collect up to 5 filenames to post
+	filenames_to_post = filenames[current_file:current_file+5]
+	current_file += 5
+
+	# Build post:
+	data = [("board", BOARD),
+	        ("thread", THREAD),
+	        ("post", "New Reply"),
+	        ("body", ""),
+	        ("subject", SUBJECT),
+	        ("name", NAME),
+	        ("no_country", HIDE_FLAG),
+	        ("json_response", "1"), #debugging
+	        ("email", EMAIL)]
+	for index, filename in enumerate(filenames_to_post):
+		filenumber = str(index + 1) if index != 0 else ""
+		data.append(("file" + filenumber, (filename, open(join(DIRECTORY, filename), "rb"))))
+
+	m = MultipartEncoderMonitor(MultipartEncoder(data), file_upload_monitor)
+
+	# Send post:
+	print("Sending post, please wait! ... ")
+	r = requests.post("https://8ch.net/post.php",
+		data=m,
+		headers = {"referer": "https://8ch.net", "Content-Type": m.content_type})
+
+	# Check if post was sent
+	#print(r.request.headers)
+	#print(r.json())
+
+	# Wait however long we need to not hit flood detection (usually 10 seconds)
+	print("Sent! Please wait for flood detection ... ", end="", flush=True)
+	for x in range(0, POST_WAIT):
+		time.sleep(1)
+		print(str(x + 1) + "... ", end="", flush=True)
+	files_left = len(filenames) - current_file
+	print("Current Iteration: " + str(current_file) + ", Files left: " + str(files_left) + " Posts left: " + str(math.ceil(files_left / 5)))
---
+++ README.md
@@ -0,0 +1,59 @@
+8chanDumper
+------------------------------
+
+A directory dumper for 8chan ([8ch.net](https://8ch.net)).
+
+Provide a directory, board and thread and 8chanDumper will post all files from the directory alphabetically up to 5 at a time using 8chan's multi-upload feature.
+
+
+Requirements
+--------------------
+* Python 3
+* Requests (https://github.com/kennethreitz/requests) and RequestsToolBelt (https://pypi.python.org/pypi/requests-toolbelt)
+
+
+Limitations & Notes
+--------------------
+* Only works for [8chan](https://8ch.net), could be tweaked to support other imageboards based off 8chan.
+* Only tested on Linux so far, it probably works with Windows.
+* 8chanDumper supports all filetypes 8chan supports, however some boards may have filetypes disabled, the program doesn't check for that yet so please be careful.
+* Will not work on boards with Captcha-per-post enabled.
+* [8chan's DNSBL](https://8ch.net/dnsbls_bypass.php) will invisibly prevent posts from getting through if it crops up, the program doesn't check for it yet so please ensure you have filled it every 50-75 or so posts.
+* Do not post from the same IP Address 8chanDumper is running from, otherwise flood detection will mess both your posts and the script up.
+* Beware of very large filesizes, at the moment 8chanDumper uses 8chan's multi-uploader to blindly upload as many as files possible per post, if the total filesize exceeds the current filesize limit on 8chan the post will not get through.
+* Irresponsible use of this script is likely to get you banned. Some sites/boards may not want you dumping. Act accordingly.
+
+
+Instructions
+--------------------
+See `python 8chanDumper -h` for details.
+
+To start, run `python 8chanDumper <Directory> <Board> <Thread>`
+
+
+If you find a bug please make a thread on [/test/](https://8ch.net/test) detailing it and I'll probably notice it.
+
+
+License
+--------------------
+MIT License
+
+Copyright (c) 2016 fixpls <fixpls@openmailbox.org>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
